6a242669228b48b3abeccad6ab5e07ff2735d23f
Derek Novak <derek.novak1@gmail.com>
2025-08-10 16:01:40 -0500

    Consolidated instrumentation files

--- vispyr_agent/profileService.js
+++
@@ -1,9 +0,0 @@
-const Pyroscope = require('@pyroscope/nodejs');
-
-Pyroscope.init({
-  serverAddress: 'http://localhost:9999',
-  appName: process.env.OTEL_SERVICE_NAME || 'node_app',
-});
-
-console.log('Starting Pyroscope Profiler');
-Pyroscope.start();
