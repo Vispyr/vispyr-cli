1f57a90afda1a80f61fded9d459cf1e7f3f968b9
Derek Novak <derek.novak1@gmail.com>
2025-08-12 14:00:01 -0500

    Updated config.alloy generator with batch routing

--- src/utils/deploy_tools/alloyService.ts
+++ src/utils/deploy_tools/alloyService.ts
@@ -68,8 +68,8 @@ otelcol.receiver.otlp "default" {
   }
 
   output {
-    traces = [ otelcol.connector.spanmetrics.default.input, otelcol.exporter.otlp.gateway_collector.input]
-    metrics = [otelcol.exporter.otlp.gateway_collector.input]
+    traces = [otelcol.connector.spanmetrics.default.input, otelcol.processor.batch.sdk_telemetry.input]
+    metrics = [otelcol.processor.batch.sdk_telemetry.input]
   }
 }
 
@@ -112,7 +112,7 @@ otelcol.connector.spanmetrics "default" {
   metrics_flush_interval = "15s"
   namespace = "traces.spanmetrics"
   output {
-    metrics = [otelcol.exporter.otlp.gateway_collector.input]
+    metrics = [otelcol.processor.batch.sdk_telemetry.input]
   }
 }
 
